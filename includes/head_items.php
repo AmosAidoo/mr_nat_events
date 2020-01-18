1d690fe46119b09c326d4dba4cdf705c3b144f25
AmosAidoo <aidooamos20@gmail.com>
2020-01-18 00:00:57 +0000

    Fixed Viewport error

--- includes/head_items.php
+++ includes/head_items.php
@@ -1,3 +1,5 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
 <!--Custom Fonts-->
 <!--Rubik Font-->
 <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
